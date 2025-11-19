273061f4983a6c5d64e503796beea979d71294a2
Cogniiq2 <popovic.lazar0603@gmail.com>
2025-11-19 19:59:07 +0100

    Updated Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -57,7 +57,7 @@ export function Navigation() {
               <img
                 src="/COGNI IQ.png"
                 alt="CogniIQ"
-                className="h-20 w-auto"
+                className="h-30 w-auto"
               />
             </motion.div>
 
